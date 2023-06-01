9cd3c241d7c30ab1254de4b0f7cd142e675a6f61
KramYengir <kramyengir@gmail.com>
2023-06-01 14:35:30 +0100

    Create function to display error msgs

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 const phoneInput = document.querySelector('#phone');
 
-
 /*
-create an array for the input elements,
-iterate thru it, adding listeners,
-on keyup, determine validity and
-add/remove invalid class accordingly
+create an array for the input elements
 */
-
 const inputElements = [...document.querySelectorAll('input')];
 
+
+/* Add eventlisteners to all inputElements,
+updating the invalid/valid class status on
+focus and each key press 
+*/
 inputElements.forEach(input =>{
   input.addEventListener('keyup', (e) =>{
     updateInvalidStatus(e.target);
@@ -20,7 +20,10 @@ inputElements.forEach(input =>{
   })
 })
 
-
+/* For the phone number field,
+we provide a more useful error msg for
+when the number is too short/long 
+ */
 phoneInput.addEventListener('keyup', (e) =>{
   if(!e.target.validity.patternMismatch){
     e.target.setCustomValidity('');
@@ -32,7 +35,10 @@ phoneInput.addEventListener('keyup', (e) =>{
   updateInvalidStatus(e.target);
 })
 
-
+/* A function that checks if the provided 
+input element is valid or not, then adding/
+removing the 'invalid' class accordingly
+ */
 const updateInvalidStatus = (input) =>{
     if(!input.validity.valid){
         input.classList.add('invalid');
@@ -43,3 +49,12 @@ const updateInvalidStatus = (input) =>{
         console.log('Valid')
     }
 }
+
+/* A simple function that adds text to a div
+which sits underneath ecah input field, used to
+display error msgs if needed
+*/
+const displayErrorMsg = (msg, inputElement) => {
+  inputElement.nextElementSibling.textContent = msg;
+}
+
